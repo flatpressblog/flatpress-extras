b184a2b2680c800cf19f716c8742f27f346ae8ca
real_nowhereman <real_nowhereman@aa359531-4221-0410-9edc-b0b45c38dadb>
2011-04-21 16:43:37 +0000

    lowering the priority to make it play nice with [html] tag; user-defined var delimiters

--- fp-plugins/macros/plugin.macros.php
+++ fp-plugins/macros/plugin.macros.php
@@ -4,11 +4,13 @@ Plugin Name: Macros
 Plugin URI: http://www.flatpress.org/
 Description: Defines some macros which are automatically expanded
 Author: NoWhereMan
-Version: 1.0
+Version: 1.1
 Author URI: http://www.nowhereland.it/
 */ 
 
 global $__plugin_macros;
+
+define('PLUGIN_MACROS_DELIM', '$');
 	
 $__plugin_macros = array(
 	'blog'		=> BLOG_BASEURL,
@@ -23,12 +25,16 @@ $__plugin_macros = array(
 function plugin_macros_expand($text) {
 	global $__plugin_macros;
 	foreach($__plugin_macros as $_macro => $_value) {
-		$text = str_replace('$'.$_macro.'$', $_value, $text);
+		$text = str_replace(
+			PLUGIN_MACROS_DELIM.$_macro.PLUGIN_MACROS_DELIM, 
+			$_value, 
+			$text
+		);
 	}
 
 	return $text;
 
 }
 
-add_filter('the_content', 'plugin_macros_expand');
+add_filter('the_content', 'plugin_macros_expand', 100);
 
