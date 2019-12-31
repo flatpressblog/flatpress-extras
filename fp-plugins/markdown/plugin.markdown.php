e3b4321c3f96824676624a3de589e165df6dba76
azett <githubregistrierung@azett.com>
2019-12-31 21:01:08 +0100

    Markdown plugin added. Thanks for creating it, Vasily - and thanks for reworking it, franah!

---
+++ fp-plugins/markdown/plugin.markdown.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Plugin Name: Markdown
+ * Version: 1.2
+ * Plugin URI: https://github.com/flatpressblog/flatpress-extras/tree/master/fp-plugins/markdown
+ * Description: Provides <a href="http://daringfireball.net/projects/markdown/">markdown</a> markup in posts
+ * Author: Vasily Polovnyov
+ * Author URI: https://github.com/vast
+ */
+
+// Openning and closing smart double-quotes.
+// define( 'SMARTYPANTS_SMART_DOUBLEQUOTE_OPEN', "&#171;" );
+// define( 'SMARTYPANTS_SMART_DOUBLEQUOTE_CLOSE', "&#187;" );
+define('XML_HTMLSAX3', plugin_getdir('markdown') . '/inc/');
+
+require plugin_getdir('markdown') . '/inc/MarkdownExtra.inc.php';
+require plugin_getdir('markdown') . '/inc/SmartyPantsTypographer.inc.php';
+use Michelf\MarkdownExtra, Michelf\SmartyPantsTypographer;
+
+// require plugin_getdir('markdown') . '/inc/safehtml.php';
+function pl_markdown($text) {
+	$my_html = MarkdownExtra::defaultTransform($text);
+	$my_html = SmartyPantsTypographer::defaultTransform($my_html);
+	return $my_html;
+}
+
+function pl_markdown_comment($text) {
+	$my_html = Markdown::defaultTransform($text);
+	$my_html = SmartyPants::defaultTransform($my_html);
+	return $my_html;
+}
+
+// markdown will do it, so remove them
+remove_filter('the_content', 'wpautop');
+remove_filter('the_content_rss', 'wpautop');
+remove_filter('the_excerpt', 'wpautop');
+remove_filter('comment_text', 'wpautop');
+remove_filter('comment_text', 'make_clickable');
+
+// remove wptexturize -- this work is already done by smartypants
+remove_filter('the_content', 'wptexturize');
+remove_filter('the_content_rss', 'wptexturize');
+remove_filter('the_excerpt', 'wptexturize');
+
+add_filter('the_content', 'pl_markdown', 1);
+add_filter('the_content_rss', 'pl_markdown', 1);
+
+add_filter('comment_text', 'pl_markdown_comment');
