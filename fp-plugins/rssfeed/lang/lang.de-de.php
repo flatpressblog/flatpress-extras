d5d26f5fb7ec73d7f06b74ea3cd6d54ad40e1197
azett <githubregistrierung@azett.com>
2023-02-04 13:46:24 +0100

    typo

--- fp-plugins/rssfeed/lang/lang.de-de.php
+++ fp-plugins/rssfeed/lang/lang.de-de.php
@@ -1,7 +1,7 @@
 <?php
 $lang ['plugin'] ['rssfeed'] = array(
-	'subject' => 'Bleibe auf dem neusten Stand!', // widget
-	'subscribe' => 'Abonieren ',
+	'subject' => 'Bleib auf dem neusten Stand!', // widget
+	'subscribe' => 'Abonnieren ',
 	);
 
 ?>
