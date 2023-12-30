6682117d3f5141d33923b2654ad7e25ee91ac4f8
Frank Hochmuth <frank.pcn@gmail.com>
2023-12-30 23:05:56 +0100

    Tag plugin added
    - Fixed: PHP8.1 Warning: trying to access array offset on value of type null

---
+++ fp-plugins/tag/lang/lang.de-de.php
@@ -0,0 +1,25 @@
+<?php
+$lang ['plugin'] ['tag'] = array(
+	'tag_s' => 'Tag(s): ',
+	'title_pre' => 'Tag: ',
+
+	'widget' => 'Schlagwörter',
+	'oneentry' => 'Ein Beitrag',
+	'entry' => ' Beitrag',
+	'entries' => ' Beiträge',
+	'notags' => 'Es gibt keine Tags.',
+
+	'related' => 'Verwandte Beiträge',
+	'norelated' => '<ul><li>Es wurde kein verwandter Beitrag gefunden.</li></ul>',
+	'error' => '<ul><li>keine Beiträge gefunden</li></ul>',
+);
+
+$lang ['plugin'] ['tag'] ['errors'] = array (
+	-1 => 'Für dieses Plugin muss BBCode geladen werden. Vielleicht wurden Plugins in der falschen Reihenfolge geladen.'
+	);
+
+$lang ['admin'] ['plugin'] ['tag'] = array(
+	'tag_pl' => 'Tag(s)',
+	'remove' => 'Entfernen',
+);
+?>
---
+++ fp-plugins/tag/lang/lang.en-us.php
@@ -0,0 +1,25 @@
+<?php
+$lang ['plugin'] ['tag'] = array(
+	'tag_s' => 'Tag(s): ',
+	'title_pre' => 'Tag: ',
+
+	'widget' => 'Tag Cloud',
+	'oneentry' => 'One entry',
+	'entry' => ' entry',
+	'entries' => ' entries',
+	'notags' => 'There isn\'t any tag.',
+
+	'related' => 'Related Entries',
+	'norelated' => '<ul><li>We haven\'t found any related entry.</li></ul>',
+	'error' => '<ul><li>Error</li></ul>',
+);
+
+$lang ['plugin'] ['tag'] ['errors'] = array(
+	-1 => 'This plugin require BBCode to be loaded. Maybe plugins have been loaded in wrong order'
+);
+
+$lang ['admin'] ['plugin'] ['tag'] = array(
+	'tag_pl' => 'Tag Plugin',
+	'remove' => 'Remove',
+);
+?>
